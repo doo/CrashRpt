fc1a80847131dcaa0a75f7932e12f48b673b6288
zexspectrum_1980@mail.ru <zexspectrum_1980@mail.ru@821a4644-a21a-0410-aec7-6199d47568cc>
2009-05-19 04:54:44 +0000

    updated
    git-svn-id: http://crashrpt.googlecode.com/svn@38 821a4644-a21a-0410-aec7-6199d47568cc

--- branches/CodeReview/src/crashrpttest/src/resource.h
+++ branches/CodeReview/src/crashrpttest/src/resource.h
@@ -1,20 +1,47 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Developer Studio generated include file.
+// Microsoft Visual C++ generated include file.
 // Used by CrashRptTest.rc
 //
 #define IDD_ABOUTBOX                    100
-#define IDS_ABOUTBOX                    101
-#define IDD_CRASHRPTTEST_DIALOG         102
 #define IDR_MAINFRAME                   128
-#define IDM_ABOUTBOX                    0x0100
+#define IDD_MAINDLG                     129
+#define IDC_MAIN_NOEXC                  1000
+#define IDC_MAIN_WIN32                  1001
+#define IDC_MAIN_TERM                   1002
+#define IDC_MAIN_UNEXP                  1003
+#define IDC_MAIN_PURECALL               1004
+#define IDC_MAIN_SECURITY               1005
+#define IDC_MAIN_INVPAR                 1006
+#define IDC_MAIN_NEW                    1007
+#define IDC_MAIN_SIGABRT                1008
+#define IDC_MAIN_SIGFPE                 1009
+#define IDC_MAIN_SIGILL                 1010
+#define IDC_MAIN_SIGINT                 1011
+#define IDC_MAIN_SIGSEGV                1012
+#define IDC_MAIN_SIGTERM                1013
+#define IDC_BUTTON14                    1026
+#define IDC_BUTTON15                    1027
+#define IDC_BUTTON16                    1028
+#define IDC_BUTTON17                    1029
+#define IDC_BUTTON18                    1030
+#define IDC_BUTTON19                    1031
+#define IDC_BUTTON20                    1032
+#define IDC_BUTTON21                    1033
+#define IDC_BUTTON22                    1034
+#define IDC_BUTTON23                    1035
+#define IDC_BUTTON24                    1036
+#define IDC_BUTTON25                    1037
+#define IDC_BUTTON26                    1038
+
+
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        132
-#define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1013
+#define _APS_NEXT_RESOURCE_VALUE        201
+#define _APS_NEXT_COMMAND_VALUE         32772
+#define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
