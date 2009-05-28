6860ea8fe0b419749c44dbd8ea0362b7297131d9
zexspectrum_1980@mail.ru <zexspectrum_1980@mail.ru@821a4644-a21a-0410-aec7-6199d47568cc>
2009-05-28 08:11:46 +0000

    APP_NAME and APP_VERSION
    git-svn-id: http://crashrpt.googlecode.com/svn@74 821a4644-a21a-0410-aec7-6199d47568cc

--- branches/CodeReview/src/crashrpttest/src/stdafx.h
+++ branches/CodeReview/src/crashrpttest/src/stdafx.h
@@ -31,3 +31,6 @@ extern CAppModule _Module;
 #else
   #pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 #endif
+
+#define APP_NAME _T("CrashRpt Tests")
+#define APP_VERSION _T("1.0.0")
