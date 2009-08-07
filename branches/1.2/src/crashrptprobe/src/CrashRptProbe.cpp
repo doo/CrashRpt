22f88e4d76d5ca938bd1060e9bf84bb415ce0d9c
zexspectrum_1980@mail.ru <zexspectrum_1980@mail.ru@821a4644-a21a-0410-aec7-6199d47568cc>
2009-08-07 16:36:10 +0000

    - add declarations for crashrptprobe API - moved all include files to /include
    git-svn-id: http://crashrpt.googlecode.com/svn@316 821a4644-a21a-0410-aec7-6199d47568cc

--- branches/1.2/src/crashrptprobe/src/CrashRptProbe.cpp
+++ branches/1.2/src/crashrptprobe/src/CrashRptProbe.cpp
@@ -7,13 +7,83 @@
 WTL::CAppModule _Module;
 
 int
-craOpenCrashReportA(
+crpOpenCrashReportW(
   LPCSTR lpszFileName,
-  LPUINT lpuHandle
+  LPUINT lpuHandle)
+{
+  return 0;
+}
+
+int
+crpOpenCrashReportA(
+  LPCSTR lpszFileName,
+  LPUINT lpuHandle)
+{
+  return 0;
+}
+
+int
+crpCloseCrashReport(
+  UINT uHandle)
+{
+  return 0;
+}
+
+CRASHRPTPROBE_API int
+crpGetStrPropertyW(
+  UINT uHandle,
+  LPCWSTR lpszPropName,
+  LPWSTR lpszBuffer,
+  UINT uBuffSize
 )
 {
   return 0;
 }
+
+CRASHRPTPROBE_API int
+crpGetStrPropertyA(
+  UINT uHandle,
+  LPCSTR lpszPropName,
+  LPSTR lpszBuffer,
+  UINT uBuffSize
+)
+{
+  return 0;
+}
+
+CRASHRPTPROBE_API int
+crpGetLongPropertyW(
+  UINT uHandle,
+  LPCWSTR lpszPropName,
+  PLONG lpnPropVal)
+{
+  return 0;
+}
+
+CRASHRPTPROBE_API int
+crpGetLongPropertyA(
+  UINT uHandle,
+  LPCSTR lpszPropName,
+  PLONG lpnPropVal)
+{
+  return 0;
+}
+
+
+CRASHRPTPROBE_API int
+crpExtractFileW(
+  UINT uHandle,
+  LPCWSTR lpszFileName,
+  LPCWSTR lpszFileSaveAs
+);
+
+CRASHRPTPROBE_API int
+crpExtractFileA(
+  UINT uHandle,
+  LPCSTR lpszFileName,
+  LPCSTR lpszFileSaveAs
+);
+
 
 // DllMain
 BOOL APIENTRY DllMain( HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
