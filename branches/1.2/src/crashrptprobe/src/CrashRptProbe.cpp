aa4feef6e669c7032c125ac2af077726103b1bf8
zexspectrum_1980@mail.ru <zexspectrum_1980@mail.ru@821a4644-a21a-0410-aec7-6199d47568cc>
2009-08-07 10:20:21 +0000

    add crashrptprobe project files
    git-svn-id: http://crashrpt.googlecode.com/svn@312 821a4644-a21a-0410-aec7-6199d47568cc

---
+++ branches/1.2/src/crashrptprobe/src/CrashRptProbe.cpp
@@ -0,0 +1,14 @@
+// CrashRptProbe.cpp : Defines the entry point for the DLL application.
+//
+
+#include "stdafx.h"
+#include "CrashRptProbe.h"
+
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+					 )
+{
+}
+
---
+++ branches/1.2/src/crashrptprobe/src/CrashRptProbe.h
@@ -0,0 +1,13 @@
+#ifndef __CRASHRPT_PROBE_H__
+#define __CRASHRPT_PROBE_H__
+
+
+#ifdef CRASHRPTPROBE_EXPORTS
+#define CRASHRPTPROBE_API __declspec(dllexport)
+#else
+#define CRASHRPTPROBE_API __declspec(dllimport)
+#endif
+
+
+
+#endif __CRASHRPT_PROBE_H__
