4696d92186e095142f0b7b1ae02902f54811140f
zexspectrum_1980@mail.ru <zexspectrum_1980@mail.ru@821a4644-a21a-0410-aec7-6199d47568cc>
2009-08-23 15:13:15 +0000

    fixed compilaton errors
    git-svn-id: http://crashrpt.googlecode.com/svn@372 821a4644-a21a-0410-aec7-6199d47568cc

--- branches/CodeReview/src/clean.bat
+++ branches/CodeReview/src/clean.bat
@@ -9,27 +9,22 @@ erase bin\*.pdb
 erase bin\*.ilk
 erase bin\*d.exe
 erase bin\*d.dll
-erase bin\*LIB.exe
 
 erase lib\*d.lib
-erase lib\*LIB.lib
 erase lib\*.exp
 
 rmdir /S /Q crashcon\src\Debug
 rmdir /S /Q crashcon\src\Release
-rmdir /S /Q "crashcon\src\Release LIB"
 erase /A H crashcon\src\*.user
 erase crashcon\src\*.aps
 
 rmdir /S /Q crashrpt\src\Debug
 rmdir /S /Q crashrpt\src\Release
-rmdir /S /Q "crashrpt\src\Release LIB"
 erase /A H crashrpt\src\*.user
 erase crashrpt\src\*.aps
 
 rmdir /S /Q CrashRptTest\src\Debug
 rmdir /S /Q CrashRptTest\src\Release
-rmdir /S /Q "CrashRptTest\src\Release LIB"
 erase /A H CrashRptTest\src\*.user
 erase CrashRptTest\src\*.aps
 
