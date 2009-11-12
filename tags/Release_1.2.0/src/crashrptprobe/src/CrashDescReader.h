dff4ffee30046e6e4c19dcd49885b73bca97284f
zexspectrum <zexspectrum@821a4644-a21a-0410-aec7-6199d47568cc>
2009-11-12 10:25:22 +0000

    git-svn-id: http://crashrpt.googlecode.com/svn@481 821a4644-a21a-0410-aec7-6199d47568cc

---
+++ tags/Release_1.2.0/src/crashrptprobe/src/CrashDescReader.h
@@ -0,0 +1,45 @@
+#pragma once
+#include "stdafx.h"
+#include <map>
+#include "tinyxml.h"
+
+class CCrashDescReader
+{
+public:
+
+  CCrashDescReader();
+  ~CCrashDescReader();
+
+  int Load(CString sFileName);
+  
+  bool m_bLoaded;
+
+  DWORD m_dwGeneratorVersion;
+  
+  CString m_sCrashGUID;
+  CString m_sAppName;
+  CString m_sAppVersion;
+  CString m_sImageName;
+  CString m_sOperatingSystem;
+  CString m_sSystemTimeUTC;
+  
+  DWORD m_dwExceptionType;
+  DWORD m_dwExceptionCode;
+  
+  DWORD m_dwFPESubcode;
+  
+  CString m_sInvParamExpression;
+  CString m_sInvParamFunction;
+  CString m_sInvParamFile;
+  DWORD m_dwInvParamLine;
+
+  CString m_sUserEmail;
+  CString m_sProblemDescription;
+
+  std::map<CString, CString> m_aFileItems;
+
+private:
+
+  int LoadXmlv10(TiXmlHandle hDoc);
+};
+
