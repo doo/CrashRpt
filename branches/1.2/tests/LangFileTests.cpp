f325554f9ada13b9306eadc104b4d8457663e6d6
zexspectrum@gmail.com <zexspectrum@gmail.com@821a4644-a21a-0410-aec7-6199d47568cc>
2011-05-15 06:23:21 +0000

    replacing relative project paths with $(SolutionDir) and $(OutDir)
    git-svn-id: http://crashrpt.googlecode.com/svn@1189 821a4644-a21a-0410-aec7-6199d47568cc

--- branches/1.2/tests/LangFileTests.cpp
+++ branches/1.2/tests/LangFileTests.cpp
@@ -88,8 +88,13 @@ void LangFileTests::Test_lang_file_versions()
   {
     CString sExePath = Utility::GetModulePath(NULL);
     CString sFileName;
+#ifndef WIN64
     sFileName.Format(_T("%s\\..\\lang_files\\crashrpt_lang_%s.ini"), 
       sExePath.GetBuffer(0), asLangAbbr[i].GetBuffer(0));
+#else
+    sFileName.Format(_T("%s\\..\\..\\lang_files\\crashrpt_lang_%s.ini"), 
+      sExePath.GetBuffer(0), asLangAbbr[i].GetBuffer(0));
+#endif //!WIN64
 
     GetPrivateProfileString(_T("Settings"), _T("CrashRptVersion"), _T(""), szBuffer, BUFF_SIZE, sFileName);
 
